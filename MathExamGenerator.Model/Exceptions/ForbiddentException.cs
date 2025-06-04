261049e742fd0bff2be54f53b20dbeb00ae7704b
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-06-04 18:17:35 +0700

    update base

---
+++ MathExamGenerator.Model/Exceptions/ForbiddentException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExamGenerator.Model.Exceptions
+{
+    public class ForbiddentException : Exception
+    {
+        public ForbiddentException(string message) : base(message)
+        {
+        }
+    }
+}
