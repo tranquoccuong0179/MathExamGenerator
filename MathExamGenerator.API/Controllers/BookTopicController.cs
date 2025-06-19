4d7465a75ee07bb721085f8b9cfea360348eac76
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-06-19 12:05:43 +0700

    GetAllChapterBySubjectBook and GetAllBookTopic

---
+++ MathExamGenerator.API/Controllers/BookTopicController.cs
@@ -0,0 +1,31 @@
+﻿
+using MathExamGenerator.API.constant;
+using MathExamGenerator.Model.Payload.Response;
+using MathExamGenerator.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using MathExamGenerator.Model.Paginate;
+using MathExamGenerator.Model.Payload.Response.BookTopic;
+
+namespace MathExamGenerator.API.Controllers
+{
+    public class BookTopicController : BaseController<BookTopicController>
+    {
+        private readonly IBookTopicService _bookService;
+
+        public BookTopicController(ILogger<BookTopicController> logger, IBookTopicService bookService) : base(logger)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet(ApiEndPointConstant.BookTopic.GetAllBookTopic)]
+        [ProducesResponseType(typeof(BaseResponse<IPaginate<GetBookTopicResponse>>), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(ProblemDetails))]
+        public async Task<IActionResult> GetAllBookTopic([FromQuery] int? page, [FromQuery] int? size)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = size ?? 10;
+            var response = await _bookService.GetAllBookTopic(pageNumber, pageSize);
+            return StatusCode(int.Parse(response.Status), response);
+        }
+    }
+}
---
+++ MathExamGenerator.Model/Payload/Response/BookTopic/GetBookTopicResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExamGenerator.Model.Payload.Response.BookTopic
+{
+    public class GetBookTopicResponse
+    {
+        public Guid Id { get; set; }
+
+        public Guid? BookChapterId { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? TopicNo { get; set; }
+    }
+}
