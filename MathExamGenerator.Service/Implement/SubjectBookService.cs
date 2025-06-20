58ab00607ac431d387c1b81ed25f6f079f987297
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-06-21 03:13:42 +0700

    Complete SubjectBook

--- MathExamGenerator.Service/Implement/SubjectBookService.cs
+++ MathExamGenerator.Service/Implement/SubjectBookService.cs
@@ -7,8 +7,10 @@ using AutoMapper;
 using MathExamGenerator.Model.Entity;
 using MathExamGenerator.Model.Exceptions;
 using MathExamGenerator.Model.Paginate;
+using MathExamGenerator.Model.Payload.Request.SubjectBook;
 using MathExamGenerator.Model.Payload.Response;
 using MathExamGenerator.Model.Payload.Response.SubjectBook;
+using MathExamGenerator.Model.Utils;
 using MathExamGenerator.Repository.Interface;
 using MathExamGenerator.Service.Interface;
 using Microsoft.AspNetCore.Http;
@@ -20,6 +22,78 @@ namespace MathExamGenerator.Service.Implement
     {
         public SubjectBookService(IUnitOfWork<MathExamGeneratorContext> unitOfWork, ILogger<SubjectBookService> logger, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(unitOfWork, logger, mapper, httpContextAccessor)
         {
+        }
+
+        public async Task<BaseResponse<CreateSubjectBookResponse>> CreateSubjectBook(CreateSubjectBookRequest request)
+        {
+            var subjectBooks = await _unitOfWork.GetRepository<SubjectBook>().GetListAsync(
+                predicate: s => s.IsActive == true);
+
+            if (subjectBooks.Any(s => s.Title.Equals(request.Title)))
+            {
+                throw new BadHttpRequestException("Tiêu đề sách môn học đã tồn tại");
+            }
+
+            var subject = await _unitOfWork.GetRepository<Subject>().SingleOrDefaultAsync(
+                predicate: s => s.Id.Equals(request.SubjectId) && s.IsActive == true) ?? throw new NotFoundException("Không tìm thấy môn học");
+
+            var subjectBook = new SubjectBook
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                FileUrl = request.FileUrl,
+                Description = request.Description,
+                SubjectId = request.SubjectId,
+                IsActive = true,
+                CreateAt = TimeUtil.GetCurrentSEATime()
+            };
+
+            await _unitOfWork.GetRepository<SubjectBook>().InsertAsync(subjectBook);
+
+            var isSuccess = await _unitOfWork.CommitAsync() > 0;
+
+            if (!isSuccess)
+            {
+                throw new Exception("Một lỗi đã xảy ra trong quá trình tạo sách môn học");
+            }
+
+            return new BaseResponse<CreateSubjectBookResponse>
+            {
+                Status = StatusCodes.Status200OK.ToString(),
+                Message = "Tạo sách cho môn học thành công",
+                Data = new CreateSubjectBookResponse
+                {
+                    Title = subjectBook.Title,
+                    Description = subjectBook.Description,
+                    FileUrl = subjectBook.FileUrl,
+                    SubjectId = subjectBook.SubjectId,
+                }
+            };
+        }
+
+        public async Task<BaseResponse<bool>> DeleteSubjectBook(Guid id)
+        {
+            var subjectBook = await _unitOfWork.GetRepository<SubjectBook>().SingleOrDefaultAsync(
+                predicate: s => s.Id.Equals(id) && s.IsActive == true) ?? throw new NotFoundException("Không tìm thấy sách môn học");
+
+            subjectBook.IsActive = false;
+            subjectBook.DeleteAt = TimeUtil.GetCurrentSEATime();
+
+            _unitOfWork.GetRepository<SubjectBook>().UpdateAsync(subjectBook);
+
+            var isSuccess = await _unitOfWork.CommitAsync() > 0;
+
+            if (!isSuccess)
+            {
+                throw new Exception("Một lỗi đã xảy ra trong quá trình xóa sách môn học");
+            }
+
+            return new BaseResponse<bool>
+            {
+                Status = StatusCodes.Status200OK.ToString(),
+                Message = "Xóa thành công",
+                Data = true
+            };
         }
 
         public async Task<BaseResponse<IPaginate<GetSubjectBookResponse>>> GetAllSubjectBookBySubject(Guid id, int page, int size)
@@ -89,6 +163,46 @@ namespace MathExamGenerator.Service.Implement
                 Status = StatusCodes.Status200OK.ToString(),
                 Message = "Lấy thông tin sách môn học thành công",
                 Data = subjectBook
+            };
+        }
+
+        public async Task<BaseResponse<GetSubjectBookResponse>> UpdateSubjectBook(Guid id, UpdateSubjectBookRequest request)
+        {
+            var subjectBook = await _unitOfWork.GetRepository<SubjectBook>().SingleOrDefaultAsync(
+                predicate: s => s.Id.Equals(id) && s.IsActive == true) ?? throw new NotFoundException("Không tìm thấy thông tin sách này");
+
+            if (request.SubjectId.HasValue)
+            {
+                var subject = await _unitOfWork.GetRepository<Subject>().SingleOrDefaultAsync(
+                    predicate: s => s.Id.Equals(request.SubjectId) && s.IsActive == true) ?? throw new NotFoundException("Không tìm thấy môn học");
+            }
+
+            subjectBook.Title = request.Title ?? subjectBook.Title;
+            subjectBook.Description = request.Description ?? subjectBook.Description;
+            subjectBook.SubjectId = request.SubjectId ?? subjectBook.SubjectId;
+            subjectBook.UpdateAt = TimeUtil.GetCurrentSEATime();
+
+            _unitOfWork.GetRepository<SubjectBook>().UpdateAsync(subjectBook);
+
+            var isSuccess = await _unitOfWork.CommitAsync() > 0;
+
+            if (!isSuccess)
+            {
+                throw new Exception("Một lỗi đã xảy ra trong quá trình cập nhật sách môn học");
+            }
+
+            return new BaseResponse<GetSubjectBookResponse>
+            {
+                Status = StatusCodes.Status200OK.ToString(),
+                Message = "Cập nhật thành công",
+                Data = new GetSubjectBookResponse
+                {
+                    Id = subjectBook.Id,
+                    Title = subjectBook.Title,
+                    Description = subjectBook.Description,
+                    FileUrl = subjectBook.FileUrl,
+                    SubjectId = subjectBook.SubjectId
+                }
             };
         }
     }
--- MathExamGenerator.Service/Interface/ISubjectBookService.cs
+++ MathExamGenerator.Service/Interface/ISubjectBookService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MathExamGenerator.Model.Paginate;
+using MathExamGenerator.Model.Payload.Request.SubjectBook;
 using MathExamGenerator.Model.Payload.Response;
 using MathExamGenerator.Model.Payload.Response.SubjectBook;
 
@@ -11,10 +12,16 @@ namespace MathExamGenerator.Service.Interface
 {
     public interface ISubjectBookService
     {
+        Task<BaseResponse<CreateSubjectBookResponse>> CreateSubjectBook(CreateSubjectBookRequest request);
+
         Task<BaseResponse<IPaginate<GetSubjectBookResponse>>> GetAllSubjectBooks(int page, int size);
 
         Task<BaseResponse<GetSubjectBookResponse>> GetSubjectBook(Guid id);
 
         Task<BaseResponse<IPaginate<GetSubjectBookResponse>>> GetAllSubjectBookBySubject(Guid id, int page, int size);
+
+        Task<BaseResponse<GetSubjectBookResponse>> UpdateSubjectBook(Guid id, UpdateSubjectBookRequest request);
+
+        Task<BaseResponse<bool>> DeleteSubjectBook(Guid id);
     }
 }
