78e86a620dbff8c82801d2c42dde1eb38091dc6f
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-06-23 15:37:52 +0700

    change folder

--- MathExamGenerator.Model/Mapper/ExamExchangeProfile.cs
+++ MathExamGenerator.Model/Mapper/ExamExchangeProfile.cs
@@ -2,6 +2,7 @@
 using MathExamGenerator.Model.Entity;
 using MathExamGenerator.Model.Payload.Request.ExamExchange;
 using MathExamGenerator.Model.Payload.Response.ExamExchange;
+using MathExamGenerator.Model.Payload.Response.Question;
 using System;
 using System.Collections.Generic;
 using System.Linq;
